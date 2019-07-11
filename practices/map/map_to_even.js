09bd1660080cdce4922505e43ddfcc2009847352
zhanggi3 <gio.zhang@oocl.com>
2019-07-11 11:21:14 +0800

    update

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,11 +1,9 @@
 'use strict';
 function map_to_even(collection){
-   let collection2=[]
-    collection.map((element)=>{
-        if(element%2===0){
-        collection.push(element);
-        }
+
+    return collection.map((element)=>{
+            return element*2;
+
         });
-   return collection2;
    }
 module.exports = map_to_even;
